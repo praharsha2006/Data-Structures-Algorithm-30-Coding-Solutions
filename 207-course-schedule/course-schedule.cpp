d16d075af5f3223e77481793d0c00eb5447a3366
praharsha2006 <cc9158@srmist.edu.in>
2026-04-16 23:42:22 +0530

    Time: 11 ms (22.10%) | Memory: 19.2 MB (81.47%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses, 0);
+
+        for (auto& pre : prerequisites) {
+            graph[pre[1]].push_back(pre[0]);
+            indegree[pre[0]]++;
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        int count = 0;
+
+        while (!q.empty()) {
+            int course = q.front();
+            q.pop();
+            count++;
+
+            for (int next : graph[course]) {
+                indegree[next]--;
+
+                if (indegree[next] == 0)
+                    q.push(next);
+            }
+        }
+
+        return count == numCourses;
+    }
+};
